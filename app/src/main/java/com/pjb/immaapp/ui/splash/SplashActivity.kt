2c5b98021b1d5fbc155010192e5f6d2882fc108c
bintangputera <sangbintang917@gmail.com>
2021-01-18 11:27:51 +0700

    Implementasi splash dan login page

---
+++ app/src/main/java/com/pjb/immaapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.pjb.immaapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pjb.immaapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
