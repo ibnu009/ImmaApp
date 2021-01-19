812053a0d022c078f0c13fb931eb107d4232755e
bintangputera <sangbintang917@gmail.com>
2021-01-19 11:59:49 +0700

    Implementasi Home (Done)

--- app/src/main/java/com/pjb/immaapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/pjb/immaapp/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.pjb.immaapp.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pjb.immaapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
